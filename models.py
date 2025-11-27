b32d3a8ff41f3c8063afa93944e27b21ac345636
Roberto Cascella <r.cascella94@gmail.com>
2025-11-27 22:36:36 +0100

    aggiornamento models

--- models.py
+++ models.py
@@ -1,8 +1,11 @@
-import bcrypt, re, random
-from sqlalchemy import Column, Date, Double, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+import random
+import bcrypt, re
+from sqlalchemy import Column, DateTime, Numeric, Integer, String, ForeignKey, func
+from sqlalchemy.orm import relationship, validates
+from enum import Enum as PyEnum
+from sqlalchemy import Enum as SqlEnum
 from database import Base
-
+from decimal import Decimal
 
 class Utente(Base):
     __tablename__ = 'utenti'
@@ -17,15 +20,24 @@ class Utente(Base):
     lavoro = relationship("Lavoro", back_populates="utenti") 
     conti = relationship("Conto", back_populates="utente", cascade="all, delete-orphan") 
 
-    def crea_codice_titolare(self):
-        prova_codice_titolare = 'CT' + str(random.randint(100000, 999999))
-        return prova_codice_titolare
+    def __init__(self, nome, cognome, codice_fiscale, lavoro_id):
+        self.nome = nome
+        self.cognome = cognome
+        self.codice_fiscale = codice_fiscale
+        self.codice_titolare = self.genera_codice_titolare()
+        self.lavoro_id = lavoro_id
+        
+    def genera_codice_titolare(self):
+        while True:
+            numero = random.randint(1, 999999)
+            codice = f"CT{numero:06d}"
 
-    def set_codice_titolare(self, codice_generato ):
-        self.codice_titolare = codice_generato
+            if not self.query.filter_by(codice_titolare=codice).first():
+                self.codice_titolare = codice
+                
 
     def crea_pin(self, pin):
-        if len(pin)<6:
+        if len(pin)<6: 
             raise ValueError("Il PIN deve essere di almeno 6 cifre.")
        
         if re.search(r"(\d)\1\1", pin): 
@@ -38,6 +50,9 @@ class Utente(Base):
        
 
     def verifica_pin(self, pin_inserito):
+        if not pin_inserito:
+            raise ValueError("PIN vuoto: impossibile verificare.")
+        
         pin_bytes = pin_inserito.encode('utf-8') #Ricontrolla automaticamente con lo stesso salt e serve per il main che controlla sia la stessa password e dovrebbe ritornare true se matchano
         db_hash_bytes = self.pin_hash.encode('utf-8')
         return bcrypt.checkpw(pin_bytes, db_hash_bytes)
@@ -48,7 +63,7 @@ class Lavoro(Base):
     __tablename__ = "lavori"
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome_lavoro = Column(String, nullable=False, unique=True)
-    stipendio_medio = Column(Double, nullable=False)
+    stipendio_mensile = Column(Numeric(10,2), nullable=False)
 
     utenti = relationship("Utente", back_populates="lavoro") #un lavoro può essere svolto da più utenti
 
@@ -60,31 +75,57 @@ class Conto(Base):
     __tablename__ = 'conti'
     id = Column(Integer, primary_key=True, autoincrement=True)
     iban = Column(String, nullable=False, unique=True)
+    saldo = Column(Numeric(10,2), nullable=False, default=Decimal("0.00"))
 
     #chiavi esterne
     utente_id = Column(Integer, ForeignKey('utenti.id'), nullable=False)
 
     #relazioni
     utente = relationship("Utente", back_populates="conti") #più conti possono appartenere ad un utente
-    transazioni = relationship("Transazione", back_populates="conto", cascade="all, delete-orphan") #un conto può avere più transazioni
+    transazioni_effettuate = relationship("Transazione", foreign_keys="Transazione.conto_mittente_id", back_populates="conto_mittente")
+
+    transazioni_ricevute = relationship("Transazione", foreign_keys="Transazione.conto_destinatario_id", back_populates="conto_destinatario")
+
+    def applica_transazione(self, transazione: "Transazione"):
+        importo = Decimal(transazione.importo)
+
+        if importo <= 0:
+            raise ValueError("L'importo deve essere positivo.")
+
+        #il conto è il MITTENTE
+        if transazione.conto_mittente_id == self.id:
+            if self.saldo < importo:
+                raise ValueError("Saldo insufficiente per eseguire la transazione.")
+            self.saldo -= importo
+
+        #il conto è il DESTINATARIO
+        if transazione.conto_destinatario_id == self.id:
+            self.saldo += importo
 
 
-    def crea_iban(self):
-        prova_iban = 'IT' + str(random.randint(100, 999)) + '00' +str(random.randint(100, 999))
-        return prova_iban
 
-    def set_iban(self, iban_generato ): #forse inutile
-        self.iban = iban_generato
 
+
+class TipoTransazione(PyEnum):
+    BONIFICO = "bonifico"  #richiede mittente e destinatario
+    DEPOSITO = "deposito"  #solo destinatario
+    PRELIEVO = "prelievo"  #solo mittente
+    PAGAMENTO = "pagamento" #come bonifico ma a un esercente
 
 class Transazione(Base):
     __tablename__ = 'transazioni'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    importo = Column(Double, nullable=False)
-    data = Column(Date, nullable=False)
+    importo = Column(Numeric(10,2), nullable=False)
+    data = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    descrizione = Column(String, nullable=True)
+    tipo = Column(SqlEnum(TipoTransazione, name="tipo_transazione"), nullable=False)
+
 
     #chiave esterna
-    conto_id = Column(Integer, ForeignKey('conti.id'), nullable=False)
+    conto_mittente_id = Column(Integer, ForeignKey('conti.id'), nullable=True)
+    conto_destinatario_id = Column(Integer, ForeignKey('conti.id'), nullable=True)
 
     #relazioni
-    conto = relationship("Conto", back_populates="transazioni") #più transazioni possono essere fatte da un conto
+    conto_mittente = relationship("Conto", foreign_keys=[conto_mittente_id], back_populates="transazioni_effettuate")
+    conto_destinatario = relationship("Conto", foreign_keys=[conto_destinatario_id], back_populates="transazioni_ricevute")
+
